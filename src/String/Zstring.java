62506cbb4625b3b3cb76b6fc470a45ee92f974be
pengruolan <pengruolan@prl.com>
2020-09-21 00:56:24 +0800

    6.字符串变换

---
+++ src/String/Zstring.java
@@ -0,0 +1,29 @@
+package String;
+import java.util.List;
+import java.util.ArrayList;
+public class Zstring {
+    public String convert(String s, int numRows) {
+        if(numRows < 2)
+            return s;
+
+        List<StringBuilder> rows = new ArrayList<StringBuilder>();
+        for(int i=0 ; i<Math.min(numRows,s.length()) ; i++){
+            rows.add(new StringBuilder());
+        }
+
+        int curRow = 0 ;
+        boolean goingDown = false;
+        for(char c : s.toCharArray()){
+            rows.get(curRow).append(c);
+            if(curRow == 0 || curRow == numRows-1)
+                goingDown = !goingDown;
+            curRow += goingDown?1:-1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for(StringBuilder row : rows)
+            ret.append(row);
+
+        return ret.toString();
+    }
+}
