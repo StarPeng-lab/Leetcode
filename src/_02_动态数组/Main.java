3c52f8f3da08e8fa229f839191fdbf2809800498
pengruolan <pengruolan@prl.com>
2020-12-27 00:53:44 +0800

    '测试'

--- src/_02_动态数组/Main.java
+++ src/_02_动态数组/Main.java
@@ -5,6 +5,7 @@ import Tools.Assert;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
+        //测试add,remove,set,get
         list.add(10);
         list.add(20);
         list.add(30);
@@ -14,6 +15,12 @@ public class Main {
         System.out.println(list);
         list.set(0,5);
         Assert.test(list.get(0)==6);
+        System.out.println(list);
+
+        //测试扩容
+        for(int i=0 ; i<30 ; i++){
+            list.add(i);
+        }
         System.out.println(list);
     }
 }
