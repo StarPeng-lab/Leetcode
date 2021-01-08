2c3847134a6bf137a63b6b0ebf46d205e4f0d311
pengruolan <pengruolan@prl.com>
2021-01-08 15:59:57 +0800

    '测试'

--- src/_05_队列/Main.java
+++ src/_05_队列/Main.java
@@ -2,6 +2,7 @@ package _05_队列;
 
 public class Main {
     public static void main(String[] args) {
+        //队列
         Queue<Integer> queue = new Queue<>();
         queue.add(11);
         queue.add(12);
@@ -10,5 +11,17 @@ public class Main {
         while(!queue.isEmpty()){
             System.out.println(queue.remove()); // 11 12 13 14
         }
+        System.out.println("-----------------------------------------------");
+
+        //双端队列
+        Deque<Integer> deque = new Deque<>();
+        deque.addFirst(11);
+        deque.addFirst(22);
+        deque.addLast(33);
+        deque.addLast(44);
+        while(!deque.isEmpty()){
+            System.out.println(deque.removeFirst()); // 头 22 11 33 44 尾
+        }
+
     }
 }
