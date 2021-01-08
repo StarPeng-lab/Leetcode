d5bca4ff8af54ee6e316a962e5feb41d6f97181b
pengruolan <pengruolan@prl.com>
2021-01-08 14:49:45 +0800

    '用LinkedList实现Queue'

---
+++ src/_05_队列/Queue.java
@@ -0,0 +1,58 @@
+package _05_队列;
+
+import _02_动态数组.List;
+import _03_链表.LinkedList;
+
+//队列是先进先出，因此要在首尾操作数据，尾进首出，用LinkedList实现
+public class Queue<E> {
+    private List<E> list = new LinkedList<E>();
+
+    /**
+     * 添加元素到队尾，即入队，出错会抛出异常
+     * 还有一个方法：boolean offer(E e),出错会返回false/null
+     * @param element
+     * @return
+     */
+    boolean add(E element){
+        if(list.add(element)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * 获取队首元素并从队列中删除，即出队，出错会抛出异常
+     * 还有一个方法：E poll(),出错会返回false/null
+     * @return
+     */
+    E remove(){
+        return list.remove(0);
+    }
+
+    /**
+     * 获取队首元素但不删除，即获取队列的头元素，出错会抛出异常
+     * 还有一个方法：E peek(),出错会返回false/null
+     * @return
+     */
+    E element(){
+        return list.get(0);
+    }
+
+    /**
+     * 获取队列长度
+     * @return
+     */
+    int size(){
+        return list.size();
+    }
+
+    /**
+     * 队列是否为空
+     * @return
+     */
+    boolean isEmpty(){
+        return list.isEmpty();
+    }
+}
+
