1745ca220158af61636715dd2043d6d5dc46be31
pengruolan <pengruolan@prl.com>
2021-01-08 20:06:02 +0800

    '循环队列'

---
+++ src/_05_队列/CircleQueue.java
@@ -0,0 +1,107 @@
+package _05_队列;
+
+//import org.omg.CORBA.Object; //Exception in thread "main" java.lang.ArrayStoreException: java.lang.Integer
+//ArrayStoreException异常是由于添加的对象类型不符合。自己new的是org,omg.CORBA.Object类，可想而知自己添加Integer类 不是Integer类的父类
+
+//循环队列，底层用数组实现，动态数组
+public class CircleQueue<E> {
+
+    private int size; //已使用的数组大小
+    private E[] elements; //数组
+    private int front; //存储队头元素的数组下标
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public CircleQueue(){
+        elements = (E[])new Object[DEFAULT_CAPACITY];
+    }
+
+    //队尾入队
+    public void addLast(E element){
+        enSureCapacity(size+1); //至少需要size+1容量，检查是否需要扩容
+        elements[index(size)] = element;
+        size++;
+    }
+
+    //队头出队
+    public E removeFirst(){
+        E frontElement = elements[front];
+        elements[front] = null;
+        front = index(1);
+        size--;
+        trim(); //检查是否需要缩容
+        return frontElement;
+    }
+
+    //获取队头元素
+    public E getFirst(){
+        return elements[front];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Capacity："+elements.length+"，Size："+size+", Front："+front+", [");
+        for(int i = 0 ; i < elements.length ; i++){
+            if(i != 0){
+                sb.append(",");
+            }
+            sb.append(elements[i]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    //循环队列中，得到真正的队头元素索引
+    private int index(int i){
+        return (front+i)%elements.length;
+    }
+
+    //扩容
+    private void enSureCapacity(int capacity){
+        int oldCapacity = elements.length;
+
+        if(oldCapacity >= capacity)
+            return;
+
+        int newCapacity = oldCapacity + (oldCapacity>>1); //注意加括号，体现优先级
+        E[] newElements = (E[])new Object[newCapacity];
+        for(int i = 0 ; i < size ; i++){
+            newElements[i] = elements[index(i)];
+        }
+        elements = newElements;
+
+        front = 0; //重置front
+
+        System.out.println(oldCapacity+"扩容为："+newCapacity);
+    }
+
+    //缩容
+    private void trim(){
+        int oldCapacity = elements.length;
+        int newCapacity = oldCapacity>>1;
+
+        if(size >= newCapacity || newCapacity <=DEFAULT_CAPACITY)
+            return;
+        if(oldCapacity <= DEFAULT_CAPACITY)
+            return;
+
+        E[] newElements = (E[])new Object[newCapacity];
+        for(int i = 0 ; i < size ; i++){
+            newElements[i] = elements[index(i)];
+        }
+        elements = newElements;
+
+        front = 0;
+
+        System.out.println(oldCapacity+"缩容为："+newCapacity+",Size："+size);
+    }
+
+}
--- src/_05_队列/Main.java
+++ src/_05_队列/Main.java
@@ -3,7 +3,7 @@ package _05_队列;
 public class Main {
     public static void main(String[] args) {
         //队列
-        Queue<Integer> queue = new Queue<>();
+      /*  Queue<Integer> queue = new Queue<>();
         queue.add(11);
         queue.add(12);
         queue.add(13);
@@ -11,9 +11,9 @@ public class Main {
         while(!queue.isEmpty()){
             System.out.println(queue.remove()); // 11 12 13 14
         }
-        System.out.println("-----------------------------------------------");
+        System.out.println("-----------------------------------------------");*/
 
-        //双端队列
+       /* //双端队列
         Deque<Integer> deque = new Deque<>();
         deque.addFirst(11);
         deque.addFirst(22);
@@ -21,6 +21,33 @@ public class Main {
         deque.addLast(44);
         while(!deque.isEmpty()){
             System.out.println(deque.removeFirst()); // 头 22 11 33 44 尾
+        }
+        System.out.println("-----------------------------------------------");*/
+
+        //循环队列
+        CircleQueue<Integer> circle = new CircleQueue<Integer>();
+        //[0,1,2,3,4,5,6,7,8,9]
+        for(int i=0 ; i<10 ; i++){
+            circle.addLast(i);
+        }
+        //[null,null,null,null,null,5,6,7,8,9]
+        for(int i=0 ; i<5; i++){
+            circle.removeFirst();
+        }
+        //[15,16,17,18,19,5,6,7,8,9]
+        //10扩容为15,15扩容为22后：[5,6,7,8,9,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,null]
+        for(int i=15 ; i<31; i++){
+            circle.addLast(i);
+        }
+        System.out.println(circle);
+
+        //22缩容为11：[null,null,null,null,null,null,null,null,null,30,null]
+        for(int i=0 ; i<20; i++){
+            circle.removeFirst();
+        }
+        System.out.println(circle);
+        while(!circle.isEmpty()){
+            System.out.println(circle.removeFirst());
         }
 
     }
