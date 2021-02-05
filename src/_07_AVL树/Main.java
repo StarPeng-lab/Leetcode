89fb08de849c1a048428bdac8c843ebf8d725265
pengruolan <pengruolan@prl.com>
2021-02-05 18:09:57 +0800

    测试

---
+++ src/_07_AVL树/Main.java
@@ -0,0 +1,35 @@
+package _07_AVL树;
+
+
+import Tools.Printer.BinaryTreeInfo;
+import Tools.Printer.BinaryTrees;
+import _06_二叉搜索树.Tree.BST;
+
+public class Main {
+    static int[] data = new int[]{85,19,69,3,7,99,95,2,1,70,44,58,11,21,14,93,57,4,56};
+
+    //测试搜索二叉树的打印
+    public static void test1(){
+        BST<Integer> bst = new BST<>();
+        for(int i=0 ; i<data.length ; i++){
+            bst.add(data[i]);
+        }
+        BinaryTrees.println(bst);
+        System.out.println("------------------------------------------------------------------------");
+    }
+
+    //测试平衡二叉树的打印
+    public static void test2(){
+        AVLTree<Integer> avlTree = new AVLTree<>();
+        for(int i=0 ; i<data.length ; i++){
+            avlTree.add(data[i]);
+        }
+        BinaryTrees.println(avlTree);
+        System.out.println("------------------------------------------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        test1();
+        test2();
+    }
+}
